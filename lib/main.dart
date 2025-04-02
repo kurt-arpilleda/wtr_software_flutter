ff048af339d966b7a3f659fc32f98156caea42ae
Kurt Bryant <BCArpilleda@gmail.com>
2025-04-02 14:56:59 +0800

    keyboard option

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Tag Search',
+      title: 'WTR Software',
       theme: ThemeData(primarySwatch: Colors.blue),
       initialRoute: initialRoute,
       routes: {
